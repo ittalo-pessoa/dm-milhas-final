f0278743263a7006bae4d3da53939327082d45cf
ToComSono2050 <kleytompedro12433@gmail.com>
2024-10-21 13:56:51 -0300

    detalhes extras

--- calculo-milheiro.js
+++ calculo-milheiro.js
@@ -1,21 +1,12 @@
 document.getElementById('calculate').addEventListener('click', function() {
-  // Pegando os valores inseridos e substituindo o ponto (.) por nada
   const valorpm = parseFloat(document.getElementById('valorpm').value.replace('.', '').replace(',', '.')) || 0;
   const quantidadepm = parseFloat(document.getElementById('quantidadepm').value.replace('.', '').replace(',', '.')) || 0;
-
-  // Pega o elemento da mensagem de erro
   const errorMessage = document.getElementById('error-message');
-  
-  // Verifica se algum dos valores é zero ou negativo
   if (valorpm <= 0 || quantidadepm <= 0) {
-      errorMessage.style.display = 'block'; // Exibe a mensagem de erro
+      errorMessage.style.display = 'block';
   } else {
-      errorMessage.style.display = 'none';  // Oculta a mensagem de erro
-
-      // Cálculo
+      errorMessage.style.display = 'none';
       const resultshow = valorpm / quantidadepm * 1000;
-
-      // Exibindo o resultado com formato adequado
       document.getElementById('resultshow').innerHTML = "R$ " + resultshow.toFixed(2).replace('.', ',');
   }
 });
