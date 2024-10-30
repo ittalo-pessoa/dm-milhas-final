509657416c9110864b1a68ca75cd217f3ecc584b
Emmanuel-Vic <nelpbz7@gmail.com>
2024-10-29 21:07:09 -0300

    adicionando a tela de acumulo

---
+++ acumuloAzul.js
@@ -0,0 +1,157 @@
+document.getElementById('calculate').addEventListener('click',function(){
+
+    const custoJan1 = parseFloat(document.getElementById('custoJan1').value);
+    const acumuloJan1 = parseFloat(document.getElementById('acumuloJan1').value);
+    const custoFev1 = parseFloat(document.getElementById('custoFev1').value);
+    const acumuloFev1 = parseFloat(document.getElementById('acumuloFev1').value);
+    const custoMar1 = parseFloat(document.getElementById('custoMar1').value);
+    const acumuloMar1 = parseFloat(document.getElementById('acumuloMar1').value);
+    const custoAbr1 = parseFloat(document.getElementById('custoAbr1').value);
+    const acumuloAbr1 = parseFloat(document.getElementById('acumuloAbr1').value);
+    const custoMai1 = parseFloat(document.getElementById('custoMai1').value);
+    const acumuloMai1 = parseFloat(document.getElementById('acumuloMai1').value);
+    const custoJun1 = parseFloat(document.getElementById('custoJun1').value);
+    const acumuloJun1 = parseFloat(document.getElementById('acumuloJun1').value);
+    const custoJul1 = parseFloat(document.getElementById('custoJul1').value);
+    const acumuloJul1 = parseFloat(document.getElementById('acumuloJul1').value);
+    const custoAgo1 = parseFloat(document.getElementById('custoAgo1').value);
+    const acumuloAgo1 = parseFloat(document.getElementById('acumuloAgo1').value);
+    const custoSet1 = parseFloat(document.getElementById('custoSet1').value);
+    const acumuloSet1 = parseFloat(document.getElementById('acumuloSet1').value);
+    const custoOut1 = parseFloat(document.getElementById('custoOut1').value);
+    const acumuloOut1 = parseFloat(document.getElementById('acumuloOut1').value);
+    const custoNov1 = parseFloat(document.getElementById('custoNov1').value);
+    const acumuloNov1 = parseFloat(document.getElementById('acumuloNov1').value);
+    const custoDez1 = parseFloat(document.getElementById('custoDez1').value);
+    const acumuloDez1 = parseFloat(document.getElementById('acumuloDez1').value);
+
+
+    const totalCusto1 = (custoJan1 + custoFev1 + custoMar1 + custoAbr1 + custoMai1 + custoJun1 + custoJul1 + custoAgo1 + custoSet1 + custoOut1 + custoNov1 + custoDez1);
+    const totalAcumulo1 = (acumuloJan1 + acumuloFev1 + acumuloMar1 + acumuloAbr1 + acumuloMai1 + acumuloJun1 + acumuloJul1 + acumuloAgo1 + acumuloSet1 + acumuloOut1 + acumuloNov1 + acumuloDez1);
+
+    document.getElementById('totalCusto1').innerHTML = (totalCusto1);
+    document.getElementById('totalAcumulo1').innerHTML = (totalAcumulo1);
+
+
+    document.getElementById('calculate2').addEventListener('click',function(){
+
+        const custoJan2 = parseFloat(document.getElementById('custoJan2').value);
+        const acumuloJan2 = parseFloat(document.getElementById('acumuloJan2').value);
+        const custoFev2 = parseFloat(document.getElementById('custoFev2').value);
+        const acumuloFev2 = parseFloat(document.getElementById('acumuloFev2').value);
+        const custoMar2 = parseFloat(document.getElementById('custoMar2').value);
+        const acumuloMar2 = parseFloat(document.getElementById('acumuloMar2').value);
+        const custoAbr2 = parseFloat(document.getElementById('custoAbr2').value);
+        const acumuloAbr2 = parseFloat(document.getElementById('acumuloAbr2').value);
+        const custoMai2 = parseFloat(document.getElementById('custoMai2').value);
+        const acumuloMai2 = parseFloat(document.getElementById('acumuloMai2').value);
+        const custoJun2 = parseFloat(document.getElementById('custoJun2').value);
+        const acumuloJun2 = parseFloat(document.getElementById('acumuloJun2').value);
+        const custoJul2 = parseFloat(document.getElementById('custoJul2').value);
+        const acumuloJul2 = parseFloat(document.getElementById('acumuloJul2').value);
+        const custoAgo2 = parseFloat(document.getElementById('custoAgo2').value);
+        const acumuloAgo2 = parseFloat(document.getElementById('acumuloAgo2').value);
+        const custoSet2 = parseFloat(document.getElementById('custoSet2').value);
+        const acumuloSet2 = parseFloat(document.getElementById('acumuloSet2').value);
+        const custoOut2 = parseFloat(document.getElementById('custoOut2').value);
+        const acumuloOut2 = parseFloat(document.getElementById('acumuloOut2').value);
+        const custoNov2 = parseFloat(document.getElementById('custoNov2').value);
+        const acumuloNov2 = parseFloat(document.getElementById('acumuloNov2').value);
+        const custoDez2 = parseFloat(document.getElementById('custoDez2').value);
+        const acumuloDez2 = parseFloat(document.getElementById('acumuloDez2').value);
+    
+        const totalCusto2 = (custoJan2 + custoFev2 + custoMar2 + custoAbr2 + custoMai2 + custoJun2 + custoJul2 + custoAgo2 + custoSet2 + custoOut2 + custoNov2 + custoDez2);
+        const totalAcumulo2 = (acumuloJan2 + acumuloFev2 + acumuloMar2 + acumuloAbr2 + acumuloMai2 + acumuloJun2 + acumuloJul2 + acumuloAgo2 + acumuloSet2 + acumuloOut2 + acumuloNov2 + acumuloDez2);
+    
+        document.getElementById('totalCusto2').innerHTML = (totalCusto2);
+        document.getElementById('totalAcumulo2').innerHTML = (totalAcumulo2);
+
+        document.getElementById('calculate3').addEventListener('click',function(){
+
+            const custoJan3 = parseFloat(document.getElementById('custoJan3').value);
+            const acumuloJan3 = parseFloat(document.getElementById('acumuloJan3').value);
+            const custoFev3 = parseFloat(document.getElementById('custoFev3').value);
+            const acumuloFev3 = parseFloat(document.getElementById('acumuloFev3').value);
+            const custoMar3 = parseFloat(document.getElementById('custoMar3').value);
+            const acumuloMar3 = parseFloat(document.getElementById('acumuloMar3').value);
+            const custoAbr3 = parseFloat(document.getElementById('custoAbr3').value);
+            const acumuloAbr3 = parseFloat(document.getElementById('acumuloAbr3').value);
+            const custoMai3 = parseFloat(document.getElementById('custoMai3').value);
+            const acumuloMai3 = parseFloat(document.getElementById('acumuloMai3').value);
+            const custoJun3 = parseFloat(document.getElementById('custoJun3').value);
+            const acumuloJun3 = parseFloat(document.getElementById('acumuloJun3').value);
+            const custoJul3 = parseFloat(document.getElementById('custoJul3').value);
+            const acumuloJul3 = parseFloat(document.getElementById('acumuloJul3').value);
+            const custoAgo3 = parseFloat(document.getElementById('custoAgo3').value);
+            const acumuloAgo3 = parseFloat(document.getElementById('acumuloAgo3').value);
+            const custoSet3 = parseFloat(document.getElementById('custoSet3').value);
+            const acumuloSet3 = parseFloat(document.getElementById('acumuloSet3').value);
+            const custoOut3 = parseFloat(document.getElementById('custoOut3').value);
+            const acumuloOut3 = parseFloat(document.getElementById('acumuloOut3').value);
+            const custoNov3 = parseFloat(document.getElementById('custoNov3').value);
+            const acumuloNov3 = parseFloat(document.getElementById('acumuloNov3').value);
+            const custoDez3 = parseFloat(document.getElementById('custoDez3').value);
+            const acumuloDez3 = parseFloat(document.getElementById('acumuloDez3').value);
+        
+            const totalCusto3 = (custoJan3 + custoFev3 + custoMar3 + custoAbr3 + custoMai3 + custoJun3 + custoJul3 + custoAgo3 + custoSet3 + custoOut3 + custoNov3 + custoDez3);
+            const totalAcumulo3 = (acumuloJan3 + acumuloFev3 + acumuloMar3 + acumuloAbr3 + acumuloMai3 + acumuloJun3 + acumuloJul3 + acumuloAgo3 + acumuloSet3 + acumuloOut3 + acumuloNov3 + acumuloDez3);
+        
+            document.getElementById('totalCusto3').innerHTML = (totalCusto3);
+            document.getElementById('totalAcumulo3').innerHTML = (totalAcumulo3);
+
+            document.getElementById('calculate4').addEventListener('click',function(){
+
+                const coBrandedOpe = parseFloat(document.getElementById('co-brandedJan').value);
+                const bancariosOpe = parseFloat(document.getElementById('bancariosJan').value);
+                const bonusOpe = parseFloat(document.getElementById('bonusJan').value);
+                const coBrandedJan = parseFloat(document.getElementById('co-brandedJan').value);
+                const bancariosJan = parseFloat(document.getElementById('bancariosJan').value);
+                const bonusJan = parseFloat(document.getElementById('bonusJan').value);
+                const coBrandedFev = parseFloat(document.getElementById('co-brandedFev').value);
+                const bancariosFev = parseFloat(document.getElementById('bancariosFev').value);
+                const bonusFev = parseFloat(document.getElementById('bonusFev').value);
+                const coBrandedMar = parseFloat(document.getElementById('co-brandedMar').value);
+                const bancariosMar = parseFloat(document.getElementById('bancariosMar').value);
+                const bonusMar = parseFloat(document.getElementById('bonusMar').value);
+                const coBrandedAbr = parseFloat(document.getElementById('co-brandedAbr').value);
+                const bancariosAbr = parseFloat(document.getElementById('bancariosAbr').value);
+                const bonusAbr = parseFloat(document.getElementById('bonusAbr').value);
+                const coBrandedMai = parseFloat(document.getElementById('co-brandedMai').value);
+                const bancariosMai = parseFloat(document.getElementById('bancariosMai').value);
+                const bonusMai = parseFloat(document.getElementById('bonusMai').value);
+                const coBrandedJun = parseFloat(document.getElementById('co-brandedJun').value);
+                const bancariosJun = parseFloat(document.getElementById('bancariosJun').value);
+                const bonusJun = parseFloat(document.getElementById('bonusJun').value);
+                const coBrandedJul = parseFloat(document.getElementById('co-brandedJul').value);
+                const bancariosJul = parseFloat(document.getElementById('bancariosJul').value);
+                const bonusJul = parseFloat(document.getElementById('bonusJul').value);
+                const coBrandedAgo = parseFloat(document.getElementById('co-brandedAgo').value);
+                const bancariosAgo = parseFloat(document.getElementById('bancariosAgo').value);
+                const bonusAgo = parseFloat(document.getElementById('bonusAgo').value);
+                const coBrandedSet = parseFloat(document.getElementById('co-brandedSet').value);
+                const bancariosSet = parseFloat(document.getElementById('bancariosSet').value);
+                const bonusSet = parseFloat(document.getElementById('bonusSet').value);
+                const coBrandedOut = parseFloat(document.getElementById('co-brandedOut').value);
+                const bancariosOut = parseFloat(document.getElementById('bancariosOut').value);
+                const bonusOut = parseFloat(document.getElementById('bonusOut').value);
+                const coBrandedNov = parseFloat(document.getElementById('co-brandedNov').value);
+                const bancariosNov = parseFloat(document.getElementById('bancariosNov').value);
+                const bonusNov = parseFloat(document.getElementById('bonusNov').value);
+                const coBrandedDez = parseFloat(document.getElementById('co-brandedDez').value);
+                const bancariosDez = parseFloat(document.getElementById('bancariosDez').value);
+                const bonusDez = parseFloat(document.getElementById('bonusDez').value);
+            
+            
+            
+                document.getElementById('exibi1').innerHTML = (totalCusto1);
+                document.getElementById('exibi2').innerHTML = (totalCusto2);
+                document.getElementById('exibi3').innerHTML = (totalCusto3);
+                
+            
+            });
+        
+        });
+    
+    });
+
+});
