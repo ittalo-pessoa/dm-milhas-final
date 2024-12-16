8ed8e7570573e9ae29a4717d66e67ef7c7f8b048
AloneM <133718204+maxx12211323232323233@users.noreply.github.com>
2024-12-16 12:58:38 -0300

    Resolvido

--- minhasMilhas.js
+++ minhasMilhas.js
@@ -8,25 +8,24 @@ function mostrarErro(campoId, mensagem) {
 }
 document.getElementById('calculate').addEventListener('click',function(){
 
-
-    const acumuloLivelo = parseFloat(localStorage.getItem('saldoAposTransf'));
-    const custoMedioLivelo = parseFloat(localStorage.getItem('custoMedioDoMilheiroDoLivelo'));
-    const acumuloEsfera = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasEs'));
-    const custoMedioEsfera = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasEs'));
-    const acumuloOu = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasOu'));
-    const custoMedioOu = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasOu'));
-    const acumuloSmiles = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasSmiles'));
-    const custoMedioSmiles = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasSmiles'));
-    const acumuloAzul = parseFloat(localStorage.getItem('saldoAposUsoDeMilhas'));
-    const custoMedioAzul = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodas'));
-    const acumuloLatam = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasLatam'));
-    const custoMedioLatam = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasLatam'));
-    const acumuloTapMilesGo = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasTapMilesGo'));
-    const custoMedioTapMilesGo = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasTapMilesGo'));
-    const acumuloIberia = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasIberia'));
-    const custoMedioIberia = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasIberia'));
-    let acumuloOutrosProgramas = parseFloat(document.getElementById('custo1').value);
-    let custoMedioProgramas = parseFloat(document.getElementById('custo2').value);
+  const acumuloLivelo = parseFloat(localStorage.getItem('saldoAposTransf')) || 0;
+  const custoMedioLivelo = parseFloat(localStorage.getItem('custoMedioDoMilheiroDoLivelo')) || 0;
+  const acumuloEsfera = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasEs')) || 0;
+  const custoMedioEsfera = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasEs')) || 0;
+  const acumuloOu = parseFloat(localStorage.getItem('saldoAposTransfOu')) || 0;
+  const custoMedioOu = parseFloat(localStorage.getItem('custoMedioDoMilheiroDoLiveloOu')) || 0;
+  const acumuloSmiles = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasSmiles')) || 0;
+  const custoMedioSmiles = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasSmiles')) || 0;
+  const acumuloAzul = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasAzul')) || 0;
+  const custoMedioAzul = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasAzul')) || 0;
+  const acumuloLatam = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasLatam')) || 0;
+  const custoMedioLatam = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasLatam')) || 0;
+  const acumuloTapMilesGo = parseFloat(localStorage.getItem('saldoAposTransfTapMiles')) || 0;
+  const custoMedioTapMilesGo = parseFloat(localStorage.getItem('custoMedioDoMilheiroTapMiles')) || 0;
+  const acumuloIberia = parseFloat(localStorage.getItem('saldoAposUsoDeMilhasIberia')) || 0;
+  const custoMedioIberia = parseFloat(localStorage.getItem('custoMedioDoMilheiroTodasIberia')) || 0;
+  let acumuloOutrosProgramas = parseFloat(document.getElementById('custo1').value) || 0;
+  let custoMedioProgramas = parseFloat(document.getElementById('custo2').value) || 0;
    
 
    
@@ -49,7 +48,10 @@ document.getElementById('calculate').addEventListener('click',function(){
   document.getElementById('acumuloLatamPass').innerHTML = "R$ " + acumuloLatam.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
   document.getElementById('custoMedioLatamPass').innerHTML = "R$ " + custoMedioLatam.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
-  document.getElementById('acumuloLatamPass').innerHTML = "R$ " + acumuloLatam.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  document.getElementById('acumuloTapMilesGo').innerHTML = "R$ " + acumuloTapMilesGo.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  document.getElementById('custoMedioTapMilesGo').innerHTML = "R$ " + custoMedioTapMilesGo.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+  
   document.getElementById('custoMedioLatamPass').innerHTML = "R$ " + custoMedioLatam.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
   document.getElementById('acumuloIberiaPlus').innerHTML = "R$ " + acumuloIberia.toLocaleString("pt-BR", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
